bbccd8ca769135aaecd36cdc07a42373630eccf1
Guilherme Pereira <125389403+guixsilva@users.noreply.github.com>
2025-03-10 21:17:05 -0300

    up

---
+++ estrutura_de_dados/apCadastroAlunos/apCadastroAlunos/Aluno.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apCadastroAlunos
+{
+    public class Aluno : IComparable<Aluno>
+    {
+        const int tamanhoRA = 5;
+        const int tamanhoNome = 30;
+        const int tamanhoNota = 4;
+        const int inicioRA = 0;
+        const int inicioNome = inicioRA + tamanhoRA;
+        const int inicioNota = inicioNome + tamanhoNome;
+
+        string ra, nome;
+        double nota;
+
+        public string Ra
+        {
+            get => ra;
+            set
+            {
+                if (value != "")
+                    ra = value.Substring(0, tamanhoRA).PadLeft(tamanhoRA, '0');
+                else
+                    throw new Exception("RA vazio é inválido");
+            }
+        }
+
+        public string Nome
+        {
+            get => nome;
+            set
+            {
+                if (value != "")
+                    nome = value.Substring(0, tamanhoRA).PadLeft(tamanhoNome, ' ');
+                else
+                    throw new Exception("Nome vazio é inválido");
+            }
+        }
+
+        public double Nota
+        {
+            get => nota;
+            set
+            {
+                if (value < 0 || value > 10)
+                    throw new Exception("Nota inválida");
+                else
+                    nota = value;
+            }
+        }
+
+
+        public Aluno(string linhaDeDados)
+        {
+            Ra = linhaDeDados.Substring(inicioRA, tamanhoRA);
+            Nome = linhaDeDados.Substring(inicioNome, tamanhoNome);
+            Nota = double.Parse(linhaDeDados.Substring(inicioNota, tamanhoNota));
+        }
+        public Aluno(string ra, string nome, float nota)
+        {
+            Ra = ra;
+            Nome = nome;
+            Nota = nota;
+        }
+
+        public int CompareTo(Aluno outroAluno)
+        {
+            return ra.CompareTo(outroAluno.ra);
+        }
+        public override string ToString()
+        {
+            return ra + " " + nome + " " + nota;
+        }
+    }
+}
---
+++ estrutura_de_dados/apCadastroAlunos/apCadastroAlunos/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace apCadastroAlunos
+{
+    public partial class FrmCadastro : Form
+    {
+
+        ListaSimples<Aluno> lista1;
+
+        public FrmCadastro()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmCadastro_Load(object sender, EventArgs e)
+        {
+            lista1 = new ListaSimples<Aluno>();
+        }
+
+        private void btnArquivo1_Click(object sender, EventArgs e)
+        {
+            FazerLeitura(ref lista1,lsb1);
+        }
+
+        private void FazerLeitura(ref ListaSimples<Aluno> qualLista, ListBox qualListBox)
+        {
+            qualLista = new ListaSimples<Aluno>(); // recria a lista a ser lida
+            if (dlgAbrir.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader arquivo = new StreamReader(dlgAbrir.FileName);
+                string linha = "";
+                while (!arquivo.EndOfStream)
+                {
+                    linha = arquivo.ReadLine();
+                    qualLista.InserirAposFim(new Aluno(linha));
+                }
+                qualLista.Listar(qualListBox);
+                arquivo.Close();
+            }
+        }
+    }
+}
