e78249e17a801d9c5f75424565cf52d967fc72bf
Guilherme Pereira <125389403+guixsilva@users.noreply.github.com>
2025-04-06 21:20:04 -0300

    up

---
+++ estrutura_de_dados/Polinomio/Polinomio/Termo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polinomio
+{
+    class Termo : IComparable<Termo>
+    {
+
+        private double coeficiente;
+        private byte expoente;
+
+        public Termo(double coeficiente, byte expoente)
+        {
+            this.coeficiente = coeficiente;
+            this.expoente = expoente;
+        }
+
+        public int CompareTo(Termo obj)
+        {
+            return expoente.CompareTo(obj.expoente);
+        }
+
+
+    }
+}
