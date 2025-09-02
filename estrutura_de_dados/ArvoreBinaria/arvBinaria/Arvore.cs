8ed833707e2c112bc2c5f280163319c356414ef3
Guilherme Pereira <125389403+guixsilva@users.noreply.github.com>
2025-09-01 21:07:50 -0300

    up

---
+++ estrutura_de_dados/ArvoreBinaria/arvBinaria/Arvore.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    public class Arvore<T> where T : IComparable<T>
+    {
+    private NoArvore<T> raiz, atual, antecessor;
+
+    public NoArvore<T> Raiz { get => raiz; set => raiz = value; }
+    public NoArvore<T> Atual { get => atual; set => atual = value; }
+    public NoArvore<T> Antecessor { get => antecessor; set => antecessor = value; }
+
+    public Arvore()
+    {
+        raiz = atual = antecessor = null;
+    }
+
+    public void Visitar(NoArvore<T> atual)
+    {
+        if (atual != null)
+        {
+            Console.WriteLine(atual.Info);
+        }
+        if (atual.Direita != null)
+        {
+            Visitar(atual.Direita);
+        }
+        if(atual.Esquerda != null)
+        {
+            Visitar(atual.Esquerda);
+        }
+        else
+        {
+            return;
+        }
+
+    }
+}
---
+++ estrutura_de_dados/ArvoreBinaria/arvBinaria/NoArvore.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    public class NoArvore<T> where T : IComparable<T>
+    {
+    T info;
+    NoArvore<T> esquerda, direita;
+    int altura;
+
+    public NoArvore(T dados, NoArvore<T> esquerdo, NoArvore<T> direita)
+    {
+        this.Info = dados;
+        this.Esquerda = esquerdo;
+        this.Direita = direita;
+    }
+
+    public NoArvore(T informacao)
+    {
+        info = informacao;
+        esquerda = direita = null;
+    }
+
+
+
+    public T Info { get => info; set => info = value; }
+    public NoArvore<T> Esquerda { get => esquerda; set => esquerda = value; }
+    public NoArvore<T> Direita { get => direita; set => direita = value; }
+
+
+}
